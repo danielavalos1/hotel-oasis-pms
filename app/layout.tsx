428fcb5867798ba99bb419629c04292bc2b08c58
Daniel Avalos <daniel_avalos@outlook.com>
2025-05-17 13:28:59 -0600

    feat: refactor layout component to include viewport metadata and clean up unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { AuthProvider } from "@/context/auth-context";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/auth-options";
 import { Toaster } from "@/components/ui/toaster";
 
 const outfit = Outfit({
@@ -16,20 +14,22 @@ const outfit = Outfit({
 export const metadata: Metadata = {
   title: "Hotel Oasis",
   description: "Sistema de gestión hotelera integral",
-  viewport: "width=device-width, initial-scale=1",
 };
 
-export default async function RootLayout({
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
+};
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await getServerSession(authOptions);
-
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={outfit.className}>
-        <AuthProvider session={session}>
+        <AuthProvider>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
